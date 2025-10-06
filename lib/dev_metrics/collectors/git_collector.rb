7176b22c0fe47fdac2e78cb862fafe7ab6a5963f
mickael-palma-wttj <mickael.palma-ext@wttj.co>
2025-10-06 12:19:01 +0100

    Refactor GitCollector and GitCommandExecutor: implement cache management delegation, enhance command execution with caching support, and streamline parameter building for Git commands.

--- lib/dev_metrics/collectors/git_collector.rb
+++ lib/dev_metrics/collectors/git_collector.rb
@@ -7,47 +7,64 @@ module DevMetrics
   module Collectors
     # Collects data from Git repositories using git log and related commands
     class GitCollector < BaseCollector
-      attr_reader :git_command, :parser, :command_cache
+      attr_reader :git_command, :parser, :cache_manager
 
       def initialize(repository, options = {})
         super
         @git_command = Utils::GitCommand.new(repository.path)
         @parser = Services::GitOutputParser.new(repository.name)
-        @command_cache = {}
-        @cache_enabled = options.fetch(:enable_cache, true)
+        @cache_manager = GitCommandCache.new(options.fetch(:enable_cache, true))
+        @command_executor = GitCommandExecutor.new(@git_command, @cache_manager)
       end
 
       def collect_commits(time_period = nil)
         setup_collection(time_period)
-        commits_output = execute_commits_command(time_period)
+        commits_output = @command_executor.execute_commits_command(time_period)
         parser.parse_commits(commits_output)
       end
 
       def collect_commit_stats(time_period = nil)
         setup_collection(time_period)
-        stats_output = execute_commit_stats_command(time_period)
+        stats_output = @command_executor.execute_commit_stats_command(time_period)
         parser.parse_commit_stats(stats_output)
       end
 
       def collect_file_changes(time_period = nil)
         setup_collection(time_period)
-        changes_output = execute_file_changes_command(time_period)
+        changes_output = @command_executor.execute_file_changes_command(time_period)
         parser.parse_file_changes(changes_output)
       end
 
       def collect_contributors(time_period = nil)
         setup_collection(time_period)
-        shortlog_output = execute_contributors_command(time_period)
+        shortlog_output = @command_executor.execute_contributors_command(time_period)
         parser.parse_contributors(shortlog_output)
       end
 
       def collect_tags
-        tags_output = execute_tags_command
+        tags_output = @command_executor.execute_tags_command
         parser.parse_tags(tags_output)
       end
 
       def collect_branches
         git_command.branch_list(all: true)
+      end
+
+      # Cache management delegation
+      def clear_cache
+        @cache_manager.clear
+      end
+
+      def cache_stats
+        @cache_manager.stats
+      end
+
+      def enable_cache
+        @cache_manager.enable
+      end
+
+      def disable_cache
+        @cache_manager.disable
       end
 
       protected
@@ -59,212 +76,243 @@ module DevMetrics
       end
 
       def perform_collection
-        # Default collection - override in specific methods
         collect_commits(@time_period)
+      end
+    end
+
+    # Handles Git command execution with caching support
+    class GitCommandExecutor
+      def initialize(git_command, cache_manager)
+        @git_command = git_command
+        @cache_manager = cache_manager
+      end
+
+      def execute_commits_command(time_period)
+        params = build_commits_params(time_period)
+        execute_cached_git_log(params, :commits)
+      end
+
+      def execute_commit_stats_command(time_period)
+        params = build_commit_stats_params(time_period)
+        execute_cached_git_log(params, :commit_stats)
+      end
+
+      def execute_file_changes_command(time_period)
+        params = build_file_changes_params(time_period)
+        execute_cached_git_log(params, :file_changes)
+      end
+
+      def execute_contributors_command(time_period)
+        params = build_contributors_params(time_period)
+        execute_cached_shortlog(params, :contributors)
+      end
+
+      def execute_tags_command
+        command = "tag -l --sort=-creatordate --format='%(refname:short)|%(creatordate)'"
+        @cache_manager.execute_with_cache(command, :tags) do
+          @git_command.execute(command, allow_pager: true)
+        end
       end
 
       private
 
-      def execute_commits_command(time_period)
-        if @cache_enabled
-          return execute_git_log_with_cache(
-            { format: '%H|%an|%ae|%ad|%s', all: true },
-            :commits,
-            time_period
-          )
+      def execute_cached_git_log(params, cache_key)
+        @cache_manager.execute_with_cache_key(cache_key, params) do
+          @git_command.git_log(params)
         end
-
-        git_command.git_log(
-          format: '%H|%an|%ae|%ad|%s',
-          since: time_period&.git_since_format,
-          until_date: time_period&.git_until_format,
-          all: true
-        )
       end
 
-      def execute_commit_stats_command(time_period)
-        if @cache_enabled
-          return execute_git_log_with_cache(
-            { format: '%H|%an|%ae|%ad|%s', numstat: true, all: true },
-            :commit_stats,
-            time_period
-          )
+      def execute_cached_shortlog(params, cache_key)
+        @cache_manager.execute_with_cache_key(cache_key, params) do
+          @git_command.shortlog(params)
         end
-
-        git_command.git_log(
-          format: '%H|%an|%ae|%ad|%s',
-          since: time_period&.git_since_format,
-          until_date: time_period&.git_until_format,
-          numstat: true,
-          all: true
-        )
       end
 
-      def execute_file_changes_command(time_period)
-        if @cache_enabled
-          return execute_git_log_with_cache(
-            { format: '%H', name_only: true, all: true },
-            :file_changes,
-            time_period
-          )
-        end
-
-        git_command.git_log(
-          format: '%H',
-          since: time_period&.git_since_format,
-          until_date: time_period&.git_until_format,
-          name_only: true,
-          all: true
-        )
+      def build_commits_params(time_period)
+        GitCommandParams.new
+          .with_format('%H|%an|%ae|%ad|%s')
+          .with_all
+          .with_time_period(time_period)
+          .to_h
       end
 
-      def execute_contributors_command(time_period)
-        if @cache_enabled
-          return execute_shortlog_with_cache(
-            { summary: true, numbered: true, all: true },
-            :contributors,
-            time_period
-          )
-        end
-
-        git_command.shortlog(
-          summary: true,
-          numbered: true,
-          all: true,
-          since: time_period&.git_since_format,
-          until_date: time_period&.git_until_format
-        )
+      def build_commit_stats_params(time_period)
+        GitCommandParams.new
+          .with_format('%H|%an|%ae|%ad|%s')
+          .with_numstat
+          .with_all
+          .with_time_period(time_period)
+          .to_h
       end
 
-      def execute_tags_command
-        execute_with_cache(
-          "tag -l --sort=-creatordate --format='%(refname:short)|%(creatordate)'",
-          :tags,
-          { allow_pager: true }
-        )
+      def build_file_changes_params(time_period)
+        GitCommandParams.new
+          .with_format('%H')
+          .with_name_only
+          .with_all
+          .with_time_period(time_period)
+          .to_h
       end
 
-      public
+      def build_contributors_params(time_period)
+        GitCommandParams.new
+          .with_summary
+          .with_numbered
+          .with_all
+          .with_time_period(time_period)
+          .to_h
+      end
+    end
 
-      # Cache management methods
-      def clear_cache
-        @command_cache.clear
-        reset_cache_stats
+    # Value object for building Git command parameters
+    class GitCommandParams
+      def initialize
+        @params = {}
       end
 
-      def invalidate_cache_for_time_period(time_period)
-        return unless @cache_enabled
-
-        time_key = build_time_key(time_period&.git_since_format, time_period&.git_until_format)
-        @command_cache.delete_if { |key, _| key.include?(time_key) }
+      def with_format(format)
+        @params[:format] = format
+        self
       end
 
-      def cache_stats
+      def with_numstat
+        @params[:numstat] = true
+        self
+      end
+
+      def with_name_only
+        @params[:name_only] = true
+        self
+      end
+
+      def with_summary
+        @params[:summary] = true
+        self
+      end
+
+      def with_numbered
+        @params[:numbered] = true
+        self
+      end
+
+      def with_all
+        @params[:all] = true
+        self
+      end
+
+      def with_time_period(time_period)
+        return self unless time_period
+
+        @params[:since] = time_period.git_since_format
+        @params[:until_date] = time_period.git_until_format
+        self
+      end
+
+      def to_h
+        @params.dup
+      end
+    end
+
+    # Manages Git command caching with statistics
+    class GitCommandCache
+      attr_reader :cache, :enabled
+
+      def initialize(enabled = true)
+        @cache = {}
+        @enabled = enabled
+        @hits = 0
+        @misses = 0
+        @key_builder = CacheKeyBuilder.new
+      end
+
+      def execute_with_cache(command, suffix, &block)
+        return block.call unless @enabled
+
+        cache_key = @key_builder.build_command_key(command, suffix)
+        fetch_or_execute(cache_key, &block)
+      end
+
+      def execute_with_cache_key(suffix, params, &block)
+        return block.call unless @enabled
+
+        cache_key = @key_builder.build_params_key(suffix, params)
+        fetch_or_execute(cache_key, &block)
+      end
+
+      def clear
+        @cache.clear
+        reset_stats
+      end
+
+      def enable
+        @enabled = true
+      end
+
+      def disable
+        @enabled = false
+        clear
+      end
+
+      def stats
         {
-          size: @command_cache.size,
-          keys: @command_cache.keys,
+          size: @cache.size,
+          enabled: @enabled,
           hit_rate: calculate_hit_rate,
-          hits: @cache_hits || 0,
-          misses: @cache_misses || 0,
+          hits: @hits,
+          misses: @misses,
         }
       end
 
-      def enable_cache
-        @cache_enabled = true
-      end
+      private
 
-      def disable_cache
-        @cache_enabled = false
-        clear_cache
-      end
-
-      def execute_git_log_with_cache(params, cache_key_suffix, time_period)
-        # Add time period to params for git_log call
-        params_with_time = params.dup
-        params_with_time[:since] = time_period&.git_since_format
-        params_with_time[:until_date] = time_period&.git_until_format
-
-        cache_key = build_git_log_cache_key(params_with_time, cache_key_suffix)
-
-        if @command_cache.key?(cache_key)
-          @cache_hits = (@cache_hits || 0) + 1
-          return @command_cache[cache_key]
+      def fetch_or_execute(cache_key)
+        if @cache.key?(cache_key)
+          @hits += 1
+          @cache[cache_key]
+        else
+          @misses += 1
+          result = yield
+          @cache[cache_key] = result
+          result
         end
-
-        @cache_misses = (@cache_misses || 0) + 1
-        result = git_command.git_log(params_with_time)
-        @command_cache[cache_key] = result
-        result
-      end
-
-      def execute_shortlog_with_cache(params, cache_key_suffix, time_period)
-        # Add time period to params for shortlog call
-        params_with_time = params.dup
-        params_with_time[:since] = time_period&.git_since_format
-        params_with_time[:until_date] = time_period&.git_until_format
-
-        cache_key = build_shortlog_cache_key(params_with_time, cache_key_suffix)
-
-        if @command_cache.key?(cache_key)
-          @cache_hits = (@cache_hits || 0) + 1
-          return @command_cache[cache_key]
-        end
-
-        @cache_misses = (@cache_misses || 0) + 1
-        result = git_command.shortlog(params_with_time)
-        @command_cache[cache_key] = result
-        result
-      end
-
-      def execute_with_cache(command, cache_key_suffix, options = {}, time_period = nil)
-        return git_command.execute(command, options) unless @cache_enabled
-
-        cache_key = build_cache_key(command, cache_key_suffix, time_period)
-
-        if @command_cache.key?(cache_key)
-          @cache_hits = (@cache_hits || 0) + 1
-          return @command_cache[cache_key]
-        end
-
-        @cache_misses = (@cache_misses || 0) + 1
-        result = git_command.execute(command, options)
-        @command_cache[cache_key] = result
-        result
-      end
-
-      def build_git_log_cache_key(params, suffix)
-        time_key = build_time_key(params[:since], params[:until_date])
-        params_key = params.except(:since, :until_date).to_s
-        "git_log_#{suffix}_#{time_key}_#{Digest::SHA256.hexdigest(params_key)[0..8]}"
-      end
-
-      def build_shortlog_cache_key(params, suffix)
-        time_key = build_time_key(params[:since], params[:until_date])
-        params_key = params.except(:since, :until_date).to_s
-        "shortlog_#{suffix}_#{time_key}_#{Digest::SHA256.hexdigest(params_key)[0..8]}"
-      end
-
-      def build_cache_key(command, suffix, time_period)
-        time_key = time_period ? "#{time_period.git_since_format}_#{time_period.git_until_format}" : 'all_time'
-        "#{suffix}_#{time_key}_#{Digest::SHA256.hexdigest(command)[0..8]}"
-      end
-
-      def build_time_key(since_date, until_date)
-        return 'all_time' unless since_date || until_date
-
-        "#{since_date || 'beginning'}_#{until_date || 'now'}"
       end
 
       def calculate_hit_rate
-        total_requests = (@cache_hits || 0) + (@cache_misses || 0)
-        return 0.0 if total_requests.zero?
+        total = @hits + @misses
+        return 0.0 if total.zero?
 
-        ((@cache_hits || 0).to_f / total_requests * 100).round(2)
+        (@hits.to_f / total * 100).round(2)
       end
 
-      def reset_cache_stats
-        @cache_hits = 0
-        @cache_misses = 0
+      def reset_stats
+        @hits = 0
+        @misses = 0
+      end
+    end
+
+    # Builds consistent cache keys for different command types
+    class CacheKeyBuilder
+      def build_command_key(command, suffix)
+        command_hash = Digest::SHA256.hexdigest(command)[0..8]
+        "#{suffix}_#{command_hash}"
+      end
+
+      def build_params_key(suffix, params)
+        time_key = extract_time_key(params)
+        params_without_time = params.except(:since, :until_date)
+        params_hash = Digest::SHA256.hexdigest(params_without_time.to_s)[0..8]
+        "#{suffix}_#{time_key}_#{params_hash}"
+      end
+
+      private
+
+      def extract_time_key(params)
+        since_date = params[:since]
+        until_date = params[:until_date]
+
+        return 'all_time' unless since_date || until_date
+
+        "#{since_date || 'beginning'}_#{until_date || 'now'}"
       end
     end
   end
